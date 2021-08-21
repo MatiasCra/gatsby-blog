f38077435ad25d1fe0c792e991fd857561093a37
Matías Cra <cravchikm@gmail.com>
2021-08-21 15:13:41 -0300

    home

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,17 @@
-import React from "react"
-import Footer from "./footer.js"
-import Header from "./header.js"
-import "../styles/index.css"
-import * as layoutStyles from "./layout.module.css"
+import React from "react";
+import Footer from "./footer.js";
+import Header from "./header.js";
+import "../styles/index.css";
+import * as layoutStyles from "./layout.module.css";
 
-const Layout = props => {
+const Layout = (props) => {
   return (
     <div className={layoutStyles.container}>
-        <Header />
-      <div className={layoutStyles.content}>
-        {props.children}
-      </div>
+      <Header />
+      <div className={layoutStyles.content}>{props.children}</div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
-import React from "react"
-import Layout from "../components/layout.js"
-import Head from "../components/head"
+import React from "react";
+import Header from "../components/header.js";
+import Footer from "../components/footer.js";
+import Head from "../components/head";
+import gatsbyLogo from "../media/gatsby-logo.svg";
+import * as homeStyles from "./home.module.css";
 
 const IndexPage = () => {
   return (
-    <Layout>
+    <div className={homeStyles.homeLayoutContainer}>
+      <Header />
       <Head title="Home" />
-      <h1>Hello.</h1>
-      <h2>I'm learning Gatsby.</h2>
-    </Layout>
-  )
-}
+      <div className={homeStyles.homeContainer}>
+        <div className={homeStyles.homeInfo}>
+          <h1>Hello</h1>
+          <h2>This is a simple <br/> blog website <br/> made ith Gatsby.</h2>
+        </div>
+        <img
+          src={gatsbyLogo}
+          alt="gatsby-logo"
+          className={homeStyles.homeLogo}
+        />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
